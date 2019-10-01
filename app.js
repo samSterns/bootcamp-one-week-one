ee4cc3df7d950fe28937dae9da9cf8e56cb2965f
samSterns <sam.g.sterns@gmail.com>
2019-10-01 10:26:35 -0700

    rough draft

---
+++ app.js
@@ -0,0 +1,39 @@
+import { isYes } from './functions.js';
+const myButton = document.getElementById('action-button');
+
+
+myButton.onclick = () => {
+    alert('welcome to my quiz!');
+    const myConfirmation = confirm('Do you want to take my quiz?');
+    console.log(myConfirmation);
+
+    const userAnswer1 = prompt('Have I lived outside the United States? (y/n)');
+    const sanitizedAnswer1 = userAnswer1.toLowerCase().trim();
+    const userCorrect1 = isYes(sanitizedAnswer1);
+
+    if(userCorrect1) {
+        alert('Good Job!');
+    } else { 
+        alert('Whoops!');
+    }
+    const userAnswer2 = prompt('Do I eat out at the finest places in Portland? (y/n)');
+    const sanitizedAnswer2 = userAnswer2.toLowerCase().trim();
+    const userCorrect2 = isYes(sanitizedAnswer2);
+
+    if(userCorrect2) {
+        alert('Whoops!');
+    } else { 
+        alert('Good Job!');
+    }
+    const userAnswer3 = prompt('Does Zuri mean beautiful in Swahili? (y/n)');
+    const sanitizedAnswer3 = userAnswer3.toLowerCase().trim();
+    const userCorrect3 = isYes(sanitizedAnswer3);
+
+    if(userCorrect1) {
+        alert('Good Job!');
+    } else { 
+        alert('Whoops!');
+    }
+};
+
+// 
